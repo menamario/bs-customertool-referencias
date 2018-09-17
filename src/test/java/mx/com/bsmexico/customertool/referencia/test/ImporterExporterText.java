196965bda7031a6f8a414b0a30f7290024019c58
Jchr <ing.jchr2427@gmail.com>
2018-09-17 10:47:18 -0500

    Cambio de libreria para el manejo de excel: de jxl a poi

--- src/test/java/mx/com/bsmexico/customertool/referencia/test/ImporterExporterText.java
+++ src/test/java/mx/com/bsmexico/customertool/referencia/test/ImporterExporterText.java
@@ -9,14 +9,16 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import mx.com.bsmexico.customertool.api.process.ExportSource;
+import mx.com.bsmexico.customertool.api.process.ImportTarget;
 import mx.com.bsmexico.customertool.api.report.ContextReport;
 import mx.com.bsmexico.customertool.api.report.ReportDataSourceFactory;
 import mx.com.bsmexico.customertool.api.report.ReportGenerator;
 import mx.com.bsmexico.customertool.api.report.ReportType;
 import mx.com.bsmexico.customertool.referencias.plugin.Referencia;
+import mx.com.bsmexico.customertool.referencias.plugin.ReferenciaImporter;
 import mx.com.bsmexico.customertool.referencias.plugin.ReferenciaXlsxExporter;
 
-public class ReportTest {
+public class ImporterExporterText {
 
 	@Test
 	public void reportReferenciasXslxTest() {
@@ -82,6 +84,34 @@ public class ReportTest {
 		try {			
 			file.createNewFile();
 			exporter.export(file);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void importerTest() {
+		final ClassLoader classLoader = getClass().getClassLoader();
+		final File file = new File(classLoader.getResource("plain/referencias.xlsx").getFile());
+
+		final ImportTarget<Referencia> target = new ImportTarget<Referencia>() {
+
+			@Override
+			public void setData(List<Referencia> data) {
+				Assert.assertTrue(data.size() == 5);
+				Assert.assertTrue("123456".equals(data.get(0).getReferencia()));
+				Assert.assertTrue("234232".equals(data.get(1).getReferencia()));
+				Assert.assertTrue("1234567".equals(data.get(2).getReferencia()));
+				Assert.assertTrue("123".equals(data.get(3).getReferencia()));
+				Assert.assertTrue("hola".equals(data.get(4).getReferencia()));
+			}
+
+		};		
+
+		try {
+			final ReferenciaImporter importer = new ReferenciaImporter(target);
+			importer.importFile(file);
 		} catch (Exception e) {
 			e.printStackTrace();
 			Assert.fail(e.getMessage());
